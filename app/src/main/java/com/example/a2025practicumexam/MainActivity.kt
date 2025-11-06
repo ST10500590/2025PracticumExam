487d332ff24009d6c4809021ee9340f77ea3b1a5
ST10500590 <ST10500590@rcconnect.edu.za>
2025-11-06 13:23:15 +0200

    Add Movie Data

--- app/src/main/java/com/example/a2025practicumexam/MainActivity.kt
+++ app/src/main/java/com/example/a2025practicumexam/MainActivity.kt
@@ -12,36 +12,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a2025practicumexam.ui.theme._2025PracticumExamTheme
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import android.content.Intent
+import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _2025PracticumExamTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            _2025PracticumExamTheme (){
+
+                Column(  modifier= Modifier.fillMaxSize(), //to expand the wrap to fill the entire screen
+                    horizontalAlignment=Alignment.CenterHorizontally, //this is going to center the composables horizontally
+                    verticalArrangement = Arrangement.Center  )
+                {
+                    Text(text = "Welcome")
+                    Button(onClick = {
+                        var start = Intent(this@MainActivity, MainScreen::class.java)
+                        startActivity(start)
+                    })
+                    {
+                        Text("Start") // Text of the button to be "Start"
+                    }
                 }
+
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _2025PracticumExamTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/a2025practicumexam/MovieData.kt
@@ -0,0 +1,10 @@
+package com.example.a2025practicumexam
+
+object MovieData {
+    val MovieTitle=arrayListOf("\nThe GodFather","\nThe Dark Knight","\nPulp Fiction\n")
+    val Director=arrayListOf("\nFrancis Ford Coppola","\nChristopher Nolan","\nQuentin Tarantino\n")
+    val Ratings=arrayListOf("\n5","\n4","\n3\n")
+    val Comments=arrayListOf("\nA masterpiece of Cinema","\nDark, complex, and unforgettable\n")
+
+
+}
